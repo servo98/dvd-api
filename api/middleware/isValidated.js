839c6d6a9e4caf3b8fff80b5075eb8a68618502e
servo pcerda <fernandonando98@live.com.mx>
2022-05-11 18:17:33 -0500

    fix: middlware with await

--- api/middleware/isValidated.js
+++ api/middleware/isValidated.js
@@ -1,27 +1,14 @@
 const isValid = (schema) => {
-    return (req, res, next) => {
-        try {
-            await schema.validateAsync(req.body);
-            return next();
-        } catch (err) {
-            return res.status(400).json({
-                msg: err
-            });
-        }
+  return async (req, res, next) => {
+    try {
+      await schema.validateAsync(req.body);
+      return next();
+    } catch (err) {
+      return res.status(400).json({
+        msg: err,
+      });
     }
-}
+  };
+};
 
-
-export default isValid
-
-
-
-
-
-
-
-
-
-
-
-
+export default isValid;
